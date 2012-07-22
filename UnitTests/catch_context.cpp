8ab2cf313d2665537fe6e45443318bcffff6f0f8
Julian Hornich <julian.hornich@ce.stud.uni-erlangen.de>
2012-07-22 14:32:35 +0200

    git added kernel UnitTest

---
+++ UnitTests/catch_context.cpp
@@ -0,0 +1,14 @@
+
+//  Copyright (c) 2012 Julian Hornich
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <oclm/oclm.hpp>
+
+TEST_CASE( "context operators", "Test available context operators")
+{
+}
