1f9da7613f73bb30d11fcd246fbd16ea7689838d
akemp <akemp1@lsu.edu>
2012-08-17 16:51:26 -0500

    Adding additional tests and formatting the output file for better readability

--- examples/speedTest.cpp
+++ examples/speedTest.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <string>
 #include <math.h>
+#include <iomanip>
 
 #include <oclm/oclm.hpp>
 #include <oclm/util/high_resolution_timer.hpp>
@@ -40,6 +41,22 @@ void runFunction(const oclm::program& p, const int& numThreads, const std::strin
         e1.get();
 }
 
+void foutWrite(const std::vector<double> timeLocal, const std::vector<double> timeGlobal, const std::vector<double> timeVecAdd, int iters, std::ofstream& fout, int precision, int width)
+{
+    for (int i = 0; i < iters; i++)
+    {
+        int numThreads = (int)std::pow((double)2, (double)i);
+        fout << numThreads << " thread execution time:\n";
+        fout << std::setw(width) << std::setiosflags(std::ios::fixed) << std::setprecision(precision) << timeLocal[i];
+        fout << std::setw(width) << std::setiosflags(std::ios::fixed) << std::setprecision(precision) << timeGlobal[i];
+        fout << std::setw(width) << std::setiosflags(std::ios::fixed) << std::setprecision(precision) << timeVecAdd[i];
+        fout << std::endl;
+        for (int i = 0; i < width * 3; i++)
+            fout << "=";
+        fout << std::endl;
+    }
+}
+
 int main()
 {
     
@@ -50,11 +67,12 @@ int main()
     // create a program from source ... possibly a vector of sources ...
     oclm::program p(src);
     
-    std::ofstream fout;
-    fout.open("log1.txt");
     std::cout << "Initializing Part 1 of the speed test: basic memory read/write and arithmetic operations.\n";
-    fout << "BASIC TESTS\n";
-    for (int i = 0; i < 24; i++)
+    std::vector<double> timeLocal;
+    std::vector<double> timeGlobal;
+    std::vector<double> timeVecAdd;
+    const int iters = 19;
+    for (int i = 0; i < iters; i++)
     {
         int numThreads = (int)std::pow((double)2, (double)i);
         std::string function = "assignLocal";
@@ -63,31 +81,101 @@ int main()
         oclm::util::high_resolution_timer t;
 
         runFunction(p, numThreads, function);
-
-        double elapsed = t.elapsed();
-        fout << "\n" << "Basic local memory integer assignment operation with: 2^" << i << " or "
-            << numThreads << " threads completed in:\n" << elapsed << "s.\n";
+        timeLocal.push_back(t.elapsed());
         
         function = "assignGlobal";
         t.restart();
         
         runFunction(p, numThreads, function);
-        elapsed = t.elapsed();
+        timeGlobal.push_back(t.elapsed());
 
-        fout << "\n" << "Basic global memory integer assignment operation with: 2^" << i << " or "
-            << numThreads << " threads completed in:\n" << elapsed << "s.\n";
         
         t.restart();
         
         function = "vecAdd";
         runFunction(p, numThreads, function);
-        elapsed = t.elapsed();
-        fout << "\n" << "Basic global memory vector addition operation with: 2^" << i << " or "
-            << numThreads << " threads completed in:\n" << elapsed << "s.\n";
+        timeVecAdd.push_back(t.elapsed());
     }
+    std::ofstream fout;
+    fout.open("logSimple.txt");
+    fout << "BASIC TESTS\n";
+    const int width = 40;
+    const int precision = 16;
+    fout << "Basic (1x operation) tests to determine the I/O speed of the OpenCL device.\n";
+    for (int i = 1; i < width * 3; i++)
+        fout << "="; 
+    fout << std::setiosflags(std::ios::right);
+    fout << std::resetiosflags(std::ios::left);
+    fout << std::endl;
+    fout << std::setw(width) << "Local 1x Read, Global 1x Write";
+    fout << std::setw(width) << "Global 1x Read/Write";
+    fout << std::setw(width) << "Global 2x Read/1x Write";
+    fout << std::endl;
+    fout <<  "Function name in kernels/speedTest.cl:" << std::endl;
+    fout << std::setw(width) << "assignLocal";
+    fout << std::setw(width) << "assignGlobal";
+    fout << std::setw(width) << "vecAdd";
+    fout << std::endl;
+    for (int i = 0; i < width * 3; i++)
+        fout << "=";
+    fout << std::endl;
+    foutWrite(timeLocal, timeGlobal, timeVecAdd, iters, fout, precision, width);
     fout.close();
-    std::cout << "Basic performance information output to log1.txt\n";
+    std::cout << "Basic performance information output to logSimple.txt\n";
     
+    timeLocal.clear();
+    timeGlobal.clear();
+    timeVecAdd.clear();
+
+    std::cout << "Initializing Part 2 of the speed test: complex (1024x kernel repetitions) memory read/write and arithmetic operations.\n";
+    for (int i = 0; i < iters; i++)
+    {
+        int numThreads = (int)std::pow((double)2, (double)i);
+        std::string function = "assignLocalMany";
+        std::cout << "Running complex tests with " << numThreads << " threads.\n";
+
+        oclm::util::high_resolution_timer t;
+        
+        runFunction(p, numThreads, function);
+        timeLocal.push_back(t.elapsed());
+        
+        function = "assignGlobalMany";
+        t.restart();
+        
+        runFunction(p, numThreads, function);
+        timeGlobal.push_back(t.elapsed());
+        
+        t.restart();
+        
+        function = "vecAddMany";
+        runFunction(p, numThreads, function);
+        timeVecAdd.push_back(t.elapsed());
+    }
+    fout.open("logComplex.txt");
+    fout << "COMPLEX TESTS\n";
+    fout << "Complex (1024x operation) tests to determine the memory read/write speed of the OpenCL device.\n";
+    for (int i = 1; i < 90; i++)
+        fout << "="; 
+    fout << std::setiosflags(std::ios::right);
+    fout << std::resetiosflags(std::ios::left);
+    fout << std::endl;
+    fout << std::setw(width) << "Local 1024x Read, Global 1024x Write";
+    fout << std::setw(width) << "Global 1024x Read/Write";
+    fout << std::setw(width) << "Global 2048x Read/1024x Write";
+    fout << std::endl;
+    fout <<  "Function name in kernels/speedTest.cl:" << std::endl;
+    fout << std::setw(width) << "assignLocalMany";
+    fout << std::setw(width) << "assignGlobalMany";
+    fout << std::setw(width) << "vecAddMany";
+    fout << std::endl;
+    for (int i = 0; i < width * 3; i++)
+        fout << "=";
+    fout << std::endl;
+    foutWrite(timeLocal, timeGlobal, timeVecAdd, iters, fout, precision, width);
+    fout.close();
+    std::cout << "Basic performance information output to logComplex.txt\n";
+    std::cout << "Note: The basic tests are more to check the I/O rate of your OpenCL device. However, the complex tests can be very informative about the nature of memory access.\n"
+        << "The first three functions in the .cl file are the basic tests, while the remaining three are the complex tests. Note how much longer it takes to access global memory rather than local memory.\n";
     //check if we're using Windows
     #if defined(__WIN32__) || defined(_WIN32) || defined(WIN32) || defined(__WINDOWS__) || defined(__TOS_WIN__)
         //if so, pause because the console usually closes at the end of execution
--- include/oclm/kernelUtil/kernels/speedTest.cl
+++ include/oclm/kernelUtil/kernels/speedTest.cl
@@ -13,3 +13,24 @@ __kernel void vecAdd(__global int* A, __global int* B, __global int* C)
     int tid = get_global_id(0);
     C[tid] = A[tid] + B[tid];
 }
+
+__kernel void assignLocalMany(__global int* A, __global int* B, __global int* C)
+{
+    int tid = get_global_id(0);
+    int total = 0;
+    for (int i = 0; i < 1024; i++)
+        total += i;
+    C[tid] = total;
+}
+__kernel void assignGlobalMany(__global int* A, __global int* B, __global int* C)
+{
+    int tid = get_global_id(0);
+    for (int i = 0; i < 1024; i++)
+        C[tid] += A[tid];
+}
+__kernel void vecAddMany(__global int* A, __global int* B, __global int* C)
+{
+    int tid = get_global_id(0);
+    for (int i = 0; i < 1024; i++)
+        C[tid] += A[tid] + B[tid];
+}
